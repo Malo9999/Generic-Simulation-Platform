b7ecf51807ed19ebb5a914751b09f9d55857b62f
Malo <ApDkiDo@gmail.com>
2026-02-27 16:14:10 +0100

    Rebuild MarbleRace track and race-state integration (#180)

--- Assets/Simulations/MarbleRace/Track/MarbleRaceTrackRenderer.cs
+++ Assets/Simulations/MarbleRace/Track/MarbleRaceTrackRenderer.cs
@@ -2,23 +2,15 @@ using UnityEngine;
 
 public sealed class MarbleRaceTrackRenderer
 {
-    private static Material sharedRoadMaterial;
-    private static Material sharedBorderMaterial;
-    private static Material sharedStartMaterial;
+    private static Material sharedMaterial;
 
     private GameObject trackRoot;
 
-    public void Apply(SimulationSceneGraph sceneGraph, MarbleRaceTrack track)
+    public void Apply(Transform arenaRoot, MarbleRaceTrack track)
     {
-        if (sceneGraph == null || track == null || track.SampleCount == 0)
+        if (arenaRoot == null || track == null || track.SampleCount <= 0)
         {
             return;
-        }
-
-        var arenaRoot = sceneGraph.WorldRoot != null ? sceneGraph.WorldRoot.Find("ArenaRoot") : null;
-        if (arenaRoot == null)
-        {
-            arenaRoot = sceneGraph.WorldRoot;
         }
 
         var existing = arenaRoot.Find("TrackRoot");
@@ -30,51 +22,45 @@ public sealed class MarbleRaceTrackRenderer
         trackRoot = new GameObject("TrackRoot");
         trackRoot.transform.SetParent(arenaRoot, false);
 
-        var road = CreateLine("Road", trackRoot.transform, GetRoadMaterial(), new Color(0.12f, 0.12f, 0.14f, 0.96f), 5, true);
-        var left = CreateLine("LeftBorder", trackRoot.transform, GetBorderMaterial(), new Color(1f, 0.76f, 0.22f, 0.98f), 10, true);
-        var right = CreateLine("RightBorder", trackRoot.transform, GetBorderMaterial(), new Color(1f, 0.76f, 0.22f, 0.98f), 10, true);
-        var start = CreateLine("StartLine", trackRoot.transform, GetStartMaterial(), new Color(1f, 1f, 1f, 0.96f), 12, false);
-        var center = CreateLine("CenterLine", trackRoot.transform, GetStartMaterial(), new Color(1f, 1f, 1f, 0.35f), 8, true);
-
-        var avgHalf = 0f;
+        var avgHalfWidth = 0f;
         for (var i = 0; i < track.SampleCount; i++)
         {
-            avgHalf += track.HalfWidth[i];
+            avgHalfWidth += track.HalfWidth[i];
         }
 
-        avgHalf /= track.SampleCount;
-        road.widthMultiplier = avgHalf * 2f;
-        left.widthMultiplier = avgHalf * 0.12f;
-        right.widthMultiplier = avgHalf * 0.12f;
-        center.widthMultiplier = avgHalf * 0.04f;
-        start.widthMultiplier = avgHalf * 0.20f;
+        avgHalfWidth /= track.SampleCount;
 
-        var c = new Vector3[track.SampleCount];
-        var l = new Vector3[track.SampleCount];
-        var r = new Vector3[track.SampleCount];
+        var road = CreateLine("Road", trackRoot.transform, new Color(0.1f, 0.1f, 0.12f, 0.9f), 5, true, avgHalfWidth * 2f);
+        var leftBorder = CreateLine("LeftBorder", trackRoot.transform, new Color(1f, 0.86f, 0.28f, 0.98f), 10, true, avgHalfWidth * 0.12f);
+        var rightBorder = CreateLine("RightBorder", trackRoot.transform, new Color(1f, 0.86f, 0.28f, 0.98f), 10, true, avgHalfWidth * 0.12f);
+        var startLine = CreateLine("StartLine", trackRoot.transform, new Color(1f, 1f, 1f, 0.98f), 12, false, avgHalfWidth * 0.22f);
+
+        var centerPoints = new Vector3[track.SampleCount];
+        var leftPoints = new Vector3[track.SampleCount];
+        var rightPoints = new Vector3[track.SampleCount];
         for (var i = 0; i < track.SampleCount; i++)
         {
-            var p = track.Center[i];
-            c[i] = new Vector3(p.x, p.y, 0f);
-            var n = track.Normal[i] * track.HalfWidth[i];
-            l[i] = new Vector3(p.x + n.x, p.y + n.y, 0f);
-            r[i] = new Vector3(p.x - n.x, p.y - n.y, 0f);
+            var center = track.Center[i];
+            var boundaryOffset = track.Normal[i] * track.HalfWidth[i];
+            centerPoints[i] = new Vector3(center.x, center.y, 0f);
+            leftPoints[i] = new Vector3(center.x + boundaryOffset.x, center.y + boundaryOffset.y, 0f);
+            rightPoints[i] = new Vector3(center.x - boundaryOffset.x, center.y - boundaryOffset.y, 0f);
         }
 
-        road.positionCount = c.Length;
-        road.SetPositions(c);
-        left.positionCount = l.Length;
-        left.SetPositions(l);
-        right.positionCount = r.Length;
-        right.SetPositions(r);
-        center.positionCount = c.Length;
-        center.SetPositions(c);
+        road.positionCount = centerPoints.Length;
+        road.SetPositions(centerPoints);
 
-        var s0 = track.Center[0] + (track.Normal[0] * track.HalfWidth[0]);
-        var s1 = track.Center[0] - (track.Normal[0] * track.HalfWidth[0]);
-        start.positionCount = 2;
-        start.SetPosition(0, new Vector3(s0.x, s0.y, 0f));
-        start.SetPosition(1, new Vector3(s1.x, s1.y, 0f));
+        leftBorder.positionCount = leftPoints.Length;
+        leftBorder.SetPositions(leftPoints);
+
+        rightBorder.positionCount = rightPoints.Length;
+        rightBorder.SetPositions(rightPoints);
+
+        var startA = track.Center[0] + (track.Normal[0] * track.HalfWidth[0]);
+        var startB = track.Center[0] - (track.Normal[0] * track.HalfWidth[0]);
+        startLine.positionCount = 2;
+        startLine.SetPosition(0, new Vector3(startA.x, startA.y, 0f));
+        startLine.SetPosition(1, new Vector3(startB.x, startB.y, 0f));
     }
 
     public void Clear()
@@ -86,42 +72,35 @@ public sealed class MarbleRaceTrackRenderer
         }
     }
 
-    private static LineRenderer CreateLine(string name, Transform parent, Material material, Color color, int order, bool loop)
+    private static LineRenderer CreateLine(string name, Transform parent, Color color, int sortingOrder, bool loop, float width)
     {
         var go = new GameObject(name);
         go.transform.SetParent(parent, false);
+
         var lr = go.AddComponent<LineRenderer>();
-        lr.material = material;
-        lr.textureMode = LineTextureMode.Stretch;
-        lr.alignment = LineAlignment.TransformZ;
+        lr.material = GetSharedMaterial();
         lr.loop = loop;
         lr.useWorldSpace = false;
-        lr.numCornerVertices = 4;
-        lr.numCapVertices = 4;
+        lr.alignment = LineAlignment.TransformZ;
+        lr.textureMode = LineTextureMode.Stretch;
+        lr.numCapVertices = 2;
+        lr.numCornerVertices = 2;
+        lr.widthMultiplier = width;
         lr.startColor = color;
         lr.endColor = color;
-        lr.sortingOrder = order;
+        lr.sortingOrder = sortingOrder;
         return lr;
     }
 
-    private static Material GetRoadMaterial()
+    private static Material GetSharedMaterial()
     {
-        return sharedRoadMaterial ??= BuildMaterial();
-    }
+        if (sharedMaterial != null)
+        {
+            return sharedMaterial;
+        }
 
-    private static Material GetBorderMaterial()
-    {
-        return sharedBorderMaterial ??= BuildMaterial();
-    }
-
-    private static Material GetStartMaterial()
-    {
-        return sharedStartMaterial ??= BuildMaterial();
-    }
-
-    private static Material BuildMaterial()
-    {
         var shader = Shader.Find("Universal Render Pipeline/Unlit") ?? Shader.Find("Sprites/Default");
-        return new Material(shader);
+        sharedMaterial = new Material(shader);
+        return sharedMaterial;
     }
 }
