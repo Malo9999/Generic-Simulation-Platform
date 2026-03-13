a8cafff1ef70ac665cf12fa6196a6348ec783c9e
Malo <ApDkiDo@gmail.com>
2026-03-13 18:10:25 +0100

    Up

--- Assets/Simulations/NeuralSlimeMold/Editor/NeuralSlimeMoldBootstrapEditor.cs
+++ Assets/Simulations/NeuralSlimeMold/Editor/NeuralSlimeMoldBootstrapEditor.cs
@@ -5,231 +5,67 @@ using UnityEngine;
 [CustomEditor(typeof(NeuralSlimeMoldBootstrap))]
 public sealed class NeuralSlimeMoldBootstrapEditor : Editor
 {
+    SerializedProperty useArenaPreset;
+    SerializedProperty selectedArenaPreset;
+
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
 
-        DrawSection("Simulation", new[]
-        {
-            "autoStart",
-            "seed",
-            "agentCount",
-            "mapSize",
-            "trailResolution",
-            "trailDecayPerSecond",
-            "trailDiffusion"
-        });
-
-        DrawSection("Quality / Performance", new[]
-        {
-            "qualityPreset",
-            "overridePerformanceOptions",
-            "overrideFieldStepInterval",
-            "overrideFieldTextureRefreshInterval",
-            "overrideMaxVisibleAgents"
-        });
-
-        DrawSection("Stress Test", new[]
-        {
-            "useStressTestProfile",
-            "stressAgentCount",
-            "stressTrailResolution"
-        });
-
         DrawArenaPresetSection();
 
-        DrawSection("Agent Motion", new[]
-        {
-            "sensorAngleDegrees",
-            "sensorDistance",
-            "speed",
-            "turnRateDegrees",
-            "depositAmount",
-            "explorationTurnNoise"
-        });
+        EditorGUILayout.Space(10);
 
-        DrawSection("Food", new[]
-        {
-            "foodNodeCount",
-            "candidateFoodNodeCount",
-            "maxSimultaneousActiveFood",
-            "foodRespawnDelay",
-            "foodRespawnDistanceBias",
-            "outerRingSpawnBias",
-            "foodStrength",
-            "foodCapacity",
-            "consumeRadius",
-            "consumeRate",
-            "spawnFromSeed",
-            "manualFoodConfigs"
-        });
-
-        DrawSection("Colony Hub", new[]
-        {
-            "useColonyHub",
-            "colonyHub",
-            "colonyHubRadius",
-            "returnToHubWeight",
-            "returnTrailBlend",
-            "returnDepositBoost",
-            "successfulReturnDepositBurst",
-            "hubInfluenceRadius"
-        });
-
-        DrawSection("Loop Pruning", new[]
-        {
-            "nonUsefulLoopPruneStrength",
-            "nonUsefulLoopTrailThreshold",
-            "nonUsefulLoopCurvatureThreshold"
-        });
-
-        DrawSection("Network Maintenance", new[]
-        {
-            "bridgeReinforcementWeight",
-            "hubOrbitSuppression",
-            "staleCorridorDecayBoost",
-            "connectorSearchRadius",
-            "connectorSteerWeight",
-            "hubTangentialPenalty",
-            "connectorCorridorWidth",
-            "returnOrbitDepositPenalty"
-        });
-
-        DrawSection("Branch Remodeling", new[]
-        {
-            "branchSpawnChance",
-            "branchSpawnTrailThreshold",
-            "branchPromotionThreshold",
-            "branchRetractionBoost",
-            "trunkStabilityBoost",
-            "duplicateTubeSuppressionRadius"
-        });
-
-        DrawSection("World Obstacles", new[]
-        {
-            "useWorldObstacles",
-            "worldObstacles",
-            "corridorBands",
-            "obstacleAvoidanceStrength",
-            "obstaclePadding"
-        });
-
-        DrawSection("Rendering", new[]
-        {
-            "showFoodMarkers",
-            "emphasizePrimaryTubes",
-            "showExplorationBranches",
-            "showFoodStateMarkers",
-            "showActivityFocus",
-            "tubeExposure",
-            "staleTrailFade",
-            "branchAlphaBias"
-        });
-
-        DrawSection("Palette", new[]
-        {
-            "useGlowAgentShape",
-            "useFieldBlobOverlay",
-            "backgroundColor"
-        });
-
-        DrawSection("Camera Framing", new[]
-        {
-            "autoFrameCamera",
-            "adaptiveCameraFraming",
-            "cameraPadding",
-            "cameraFollowSmooth",
-            "cameraZoomSmooth",
-            "minimumCameraSize",
-            "cameraLookAheadToActivity",
-            "cameraDeadZoneRadius"
-        });
-
-        EditorGUILayout.Space(10f);
-        DrawRuntimeButtons();
+        DrawDefaultInspectorExceptArena();
 
         serializedObject.ApplyModifiedProperties();
     }
 
-    private void DrawArenaPresetSection()
+    void OnEnable()
     {
-        EditorGUILayout.Space(6f);
-        EditorGUILayout.LabelField("Arena Presets", EditorStyles.boldLabel);
+        useArenaPreset = serializedObject.FindProperty("useArenaPreset");
+        selectedArenaPreset = serializedObject.FindProperty("selectedArenaPreset");
+    }
 
-        var usePresetProp = serializedObject.FindProperty("useArenaPreset");
-        var selectedPresetProp = serializedObject.FindProperty("selectedArenaPreset");
+    void DrawArenaPresetSection()
+    {
+        EditorGUILayout.BeginVertical("box");
 
-        if (usePresetProp != null)
+        EditorGUILayout.LabelField("Arena Presets (Pass A)", EditorStyles.boldLabel);
+
+        EditorGUILayout.PropertyField(useArenaPreset, new GUIContent("Use Arena Preset"));
+
+        if (useArenaPreset.boolValue)
         {
-            EditorGUILayout.PropertyField(usePresetProp);
-        }
+            EditorGUILayout.PropertyField(selectedArenaPreset, new GUIContent("Preset"));
 
-        if (selectedPresetProp != null)
-        {
-            EditorGUILayout.PropertyField(selectedPresetProp);
-        }
+            NeuralSlimeMoldBootstrap bootstrap = (NeuralSlimeMoldBootstrap)target;
 
-        if (usePresetProp != null && usePresetProp.boolValue)
-        {
-            EditorGUILayout.HelpBox("Arena presets overwrite hub/food/obstacle layout for this bootstrap.", MessageType.Info);
-
-            var bootstrap = target as NeuralSlimeMoldBootstrap;
-            if (bootstrap != null && GUILayout.Button("Apply Arena Preset"))
+            if (GUILayout.Button("Apply Arena Preset"))
             {
+                Undo.RecordObject(bootstrap, "Apply Arena Preset");
                 bootstrap.ApplyArenaPreset();
                 EditorUtility.SetDirty(bootstrap);
             }
+
+            EditorGUILayout.HelpBox(
+                "Arena presets generate hub, food nodes, obstacles and corridor bands automatically. " +
+                "Disable this to return to manual configuration.",
+                MessageType.Info
+            );
         }
+
+        EditorGUILayout.EndVertical();
     }
 
-    private void DrawSection(string title, string[] propertyNames)
+    void DrawDefaultInspectorExceptArena()
     {
-        EditorGUILayout.Space(6f);
-        EditorGUILayout.LabelField(title, EditorStyles.boldLabel);
-
-        for (var i = 0; i < propertyNames.Length; i++)
-        {
-            Draw(propertyNames[i]);
-        }
-    }
-
-    private void DrawRuntimeButtons()
-    {
-        var bootstrap = target as NeuralSlimeMoldBootstrap;
-        if (bootstrap == null)
-        {
-            return;
-        }
-
-        using (new EditorGUILayout.HorizontalScope())
-        {
-            if (GUILayout.Button("Start / Reset Simulation"))
-            {
-                bootstrap.StartSimulation();
-                EditorUtility.SetDirty(bootstrap);
-            }
-
-            if (GUILayout.Button("Reseed"))
-            {
-                bootstrap.Reseed();
-                EditorUtility.SetDirty(bootstrap);
-            }
-
-            if (GUILayout.Button("Apply Stress Profile"))
-            {
-                bootstrap.ApplyStressTestProfile();
-                EditorUtility.SetDirty(bootstrap);
-            }
-        }
-    }
-
-    private void Draw(string propertyName)
-    {
-        var prop = serializedObject.FindProperty(propertyName);
-        if (prop != null)
-        {
-            EditorGUILayout.PropertyField(prop, true);
-        }
+        DrawPropertiesExcluding(
+            serializedObject,
+            "m_Script",
+            "useArenaPreset",
+            "selectedArenaPreset"
+        );
     }
 }
 #endif
